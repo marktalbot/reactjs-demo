8a011169401cbc7f8f208038c661db0052dc6c8f
Mark Talbot <markjtalbot@gmail.com>
2016-11-26 19:19:08 -0500

    Importing JS helper file and generating store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { getFunName } from './../helpers.js';
 
 class StorePicker extends React.Component {
+    
+    goToStore() {
+        console.log('here');
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore()}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" placeholder="Store Name" />
+                <input type="text" defaultValue={getFunName()} placeholder="Store Name" />
                 <button type="submit">Visit Store</button>
             </form>
         )
